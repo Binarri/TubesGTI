dfbdcc42a96cfbbe779b8a1d2c2a2e8d9d921187
Binarri <binarridhawiritanaya0@gmail.com>
2026-06-02 17:43:30 +0700

    Delete MINECRAFT PIN JAM directory

--- MINECRAFT PIN JAM/ui.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void renderTextJumbo(float x, float y, float scale, float thickness, const char* text);
-float getTextWidth(float scale, const char* text);
-void renderTextCentered(float y, float scale, float thickness, const char* text);
-void drawPanel(float x1, float y1, float x2, float y2, float br, float bg, float bb, float ba, float lr, float lg, float lb, float lineW);
-
-#endif
